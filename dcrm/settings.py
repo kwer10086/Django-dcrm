13c93ba815979cfb260c6838464a6ad117ee7f53
kwer10086 <kwer10086@gmail.com>
2023-04-24 15:02:57 +0800

    registration

--- dcrm/settings.py
+++ dcrm/settings.py
@@ -43,7 +43,7 @@ ROOT_URLCONF = 'dcrm.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['Templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- website/views.py
+++ website/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login,logout,authenticate
+from django.contrib import messages
 
 # Create your views here.
+
+
+def home(request):
+    # check to see if logging in
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        # authenticate
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request, user)
+            messages.success(request,'You Have Been Logged in!')
+            return redirect('home')
+        else:
+            messages.error(request, 'Username or Password error...')
+            return redirect('home')
+    else:
+        return render(request,'home.html',{})
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request,'you have been logged out...')
+    return redirect('home')
+
+
+def register(request):
+
+
+
+    return render(request, 'register.html', {})
